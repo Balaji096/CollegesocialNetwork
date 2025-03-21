8c74edd2af8c2e93148e6b3911d595b42276624b
K. Venkata Balaji <balaji.kanthuri22@gmail.com>
2025-03-21 11:45:45 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php")
+?>
+
+  <section id="contentSection">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="left_content">
+          <div class="contact_area">
+            <h2>About Us</h2>
+            <h1>CMR College of Engineering & Technology</h1>
+            <p>CMR Institute of Technology is one of the best engineering Colleges for aspiring engineering students. 
+              It is one of the three colleges established by MGR Educational Society. 
+              CMR Institute of Technology was established in 2005 in 10 Acres and built up area of 31132.72 Sq.M. with a single – minded aim to provide a perfect platform to students in the field of Engineering, Technology and Management for their academic and overall personality development. 
+              The College has a rich tradition of soaring high with academic excellence & overall personal growth of students.  </p>
+             
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+ <?php
+ include("footer.php")
+ ?>
---
+++ datatables.php
@@ -0,0 +1,54 @@
+<link rel="stylesheet" type="text/css" href="DataTables/media/css/dataTables.bootstrap.css">
+<link rel="stylesheet" type="text/css" href="DataTables/examples/resources/syntax/shCore.css">
+<style type="text/css" class="init"></style>
+<script type="text/javascript" language="javascript" src="DataTables/media/js/jquery.dataTables.js"></script>
+<script type="text/javascript" language="javascript" src="DataTables/media/js/dataTables.bootstrap.js"></script>
+<script type="text/javascript" language="javascript" src="DataTables/examples/resources/syntax/shCore.js"></script>
+<script type="text/javascript" language="javascript" class="init">	
+$(document).ready(function() {
+	$('#example').DataTable();
+} );
+</script>
+<!--
+			<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Position</th>
+						<th>Office</th>
+						<th>Age</th>
+						<th>Start date</th>
+						<th>Salary</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<th>Name</th>
+						<th>Position</th>
+						<th>Office</th>
+						<th>Age</th>
+						<th>Start date</th>
+						<th>Salary</th>
+					</tr>
+				</tfoot>
+				<tbody>
+					<tr>
+						<td>Tiger Nixon</td>
+						<td>System Architect</td>
+						<td>Edinburgh</td>
+						<td>61</td>
+						<td>2011/04/25</td>
+						<td>$320,800</td>
+					</tr>
+					<tr>
+						<td>Garrett Winters</td>
+						<td>Accountant</td>
+						<td>Tokyo</td>
+						<td>63</td>
+						<td>2011/07/25</td>
+						<td>$170,750</td>
+					</tr>
+					<tr>
+				</tbody>
+			</table>
+-->
